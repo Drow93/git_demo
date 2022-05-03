a2b410fe463b541a6d94edf6346555bd79cb47ee
onion <1003160513@qq.com>
2022-05-03 18:10:52 +0800

    fifth-fix-commit

--- src/Hello.java
+++ src/Hello.java
@@ -5,8 +5,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("第一条信息");
         System.out.println("第二条信息");
-        System.out.println("第三条信息");
-        System.out.println("第四条信息");
-        System.out.println("我是hot-fix");
+        System.out.println("fix");
     }
 }
