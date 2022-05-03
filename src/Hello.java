7d34711f60e69004c863d7cff953f5bddf629708
onion <1003160513@qq.com>
2022-05-03 17:58:22 +0800

    first-master-commit

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * @author onion_Knight
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("第一条信息");
+        System.out.println("第二条信息");
+        System.out.println("第三条信息");
+    }
+}
