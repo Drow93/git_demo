e9a73537df45b5b046b587eeff158fc8d4876eba
onion <1003160513@qq.com>
2024-10-30 22:26:39 +0800

    feat: create User

---
+++ User.java
@@ -0,0 +1,46 @@
+/**
+* @program: git_demo
+* @ClassName: User
+* @description: 
+* @author: ZJP
+* @create: 2024-10-30 22:18
+* @Version 1.0
+**/
+public class User {
+
+    private String name;
+
+    private int age;
+
+    private String address;
+
+    public User(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
